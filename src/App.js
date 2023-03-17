f854e7c3aa8645af724bc766e53eca52df7c3015
rachelblackie <rachelannblackie@gmail.com>
2023-03-17 10:56:14 +0100

    changes to footer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ function App() {
     <div className="App">
       <Weather defaultCity="Valencia" />
       <footer>
-        <a href="https://github.com/rachelblackie/react-weather-app">
+        <a
+          href="https://github.com/rachelblackie/react-weather-app"
+          rel="noopener noreferrer"
+        >
           Open-source code
         </a>{" "}
         by{" "}
-        <a href="https://www.linkedin.com/in/rachel-blackie-273b18127/">
+        <a
+          href="https://www.linkedin.com/in/rachel-blackie-273b18127/"
+          rel="noopener noreferrer"
+        >
           Rachel Blackie
         </a>
       </footer>
